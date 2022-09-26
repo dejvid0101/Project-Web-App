cc1cbcb1c0dfb1a420cb27e3c47abcb16a2e4d41
David Koren <dejvid0101@gmail.com>
2022-09-27 00:10:34 +0200

    client side database connection successfully created

---
+++ Client Side/Models/IdentityModels.cs
@@ -0,0 +1,45 @@
+﻿using System.Data.Entity;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace Client_Side.Models
+{
+    public class Role : IdentityRole<int, UserRole>
+    {
+        public Role() { }
+        public Role(string name) { Name = name; }
+    }
+    public class UserRole : IdentityUserRole<int> { }
+    public class UserClaim : IdentityUserClaim<int> { }
+    public class UserLogin : IdentityUserLogin<int> { }
+    // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit https://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
+    public class AppUser : IdentityUser<int,
+UserLogin, UserRole, UserClaim>
+
+    {
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<AppUser,int> manager)
+        {
+            // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
+            var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+            // Add custom user claims here
+            return userIdentity;
+        }
+    }
+
+    public class RwaIdentityDbContext : IdentityDbContext<AppUser, Role,
+int, UserLogin, UserRole, UserClaim>
+
+    {
+        public RwaIdentityDbContext()
+            : base("DefaultConnection")
+        {
+        }
+
+        public static RwaIdentityDbContext Create()
+        {
+            return new RwaIdentityDbContext();
+        }
+    }
+}
---
+++ Client Side/Startup.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Owin;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+[assembly: OwinStartup(typeof(Client_Side.App_Start.Startup))]
+
+namespace Client_Side.App_Start
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
